f16ece7ca7ce1e879a7b4bd73e489c9153a0f0ff
plehnert <plehnert@uni-bremen.de>
2024-10-18 16:55:12 +0200

    Quizstart

--- Main.py
+++ Main.py
@@ -7,13 +7,13 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, "
                "answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT)")
 
-cursor.execute("INSERT INTO questions (question, answer1, answer2, answer3, answer4) "
-               "VALUES ('Wer war der erste Kaiser Roms?', 'Augustus', 'Marcus Aurelius', 'Nero', 'Julius Caesar')")
-
 conn.commit()
 
-cursor.execute("SELECT * FROM questions")
-questions = cursor.fetchall()
+mode = input("Willkommen bei QuizPY! Schreib '1' für den Quiz-Modus, um direkt anzufangen mit dem Quiz oder '2' falls du zusätzliche Fragen hinzufügen möchtest!\n")
 
-for question in questions:
-    print(question)
+if mode == '1':
+    print()
+elif mode == '2':
+    print()
+else:
+    print()
